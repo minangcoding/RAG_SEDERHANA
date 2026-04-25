de4ad91ddc4ecf4f1394c998e279d3873e84d3c7
Refido Arjunal Akmal <refidoarjunalakmal@gmail.com>
2026-04-25 13:29:27 +0700

    Merubah Model Grok

--- api/index.py
+++ api/index.py
@@ -54,7 +54,7 @@ def chat():
                 {"role": "system", "content": system_prompt},
                 {"role": "user", "content": user_query}
             ],
-            model="llama3-8b-8192",
+            model="llama-3.1-8b-instant",
             temperature=0.3,
         )
 
